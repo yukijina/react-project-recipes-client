98b919e3251846b2f833cbe34ba3bd42459b58f3
yukijina <yukijina@gmail.com>
2019-09-29 17:34:08 -0700

    import recipesReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import recipesReducer from './recipesReducer.js';
 
 const rootReducer = combineReducers({
-    
+    recipesReducer
 })
 
 export default rootReducer
