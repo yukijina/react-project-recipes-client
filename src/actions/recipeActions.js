dd1b2ceddb45b0956dfda49f0da35936ad0aa918
yukijina <yukijina@gmail.com>
2019-09-29 17:35:52 -0700

    create function for loading browser and fetching data from api

---
+++ src/actions/recipeActions.js
@@ -0,0 +1,33 @@
+export const loadingRecipes = () => {
+    return {
+        type: 'LOADING_RECIPES'
+    }
+}
+
+export const sendingRecipes = recipes => {
+    const recipeData = recipes.map(recipe => {
+        return {
+            title: recipe.title, 
+            recipeId: recipe.id,
+            image: recipe.image,
+            steps: recipe.analyzedInstructions[0].steps,
+            ingredients: recipe.extendedIngredients
+        }
+    })
+    return {
+        type: 'FETCH_RECIPES',
+        payload: recipeData
+    }
+}
+
+export const fetchRecipes = ()=> {
+    const API_KEY = process.env.REACT_APP_APIKEY;
+    return (dispatch) => {
+        dispatch(loadingRecipes())
+        return fetch(`https://api.spoonacular.com/recipes/random?number=3&apiKey=${API_KEY}`)
+        .then(resp => resp.json())
+        .then(recipeColletctions => dispatch(sendingRecipes(recipeColletctions.recipes)))
+    }
+} 
+
+
