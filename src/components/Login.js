7afc6df3983676d0e9ee21eaef23798bf5bd2bff
yukijina <yukijina@gmail.com>
2019-10-01 06:54:31 -0700

    add login form - create andleChange and handleSubmit

---
+++ src/components/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { updateLoginForm }from '../actions/loginForm.js';
+import { login } from '../actions/currentUsers.js';
+
+const Login = ({loginData, updateLoginForm, login}) => {
+    const handleChange = event => {
+        updateLoginForm({...loginData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        login(loginData)
+    }
+    return (    
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={loginData.username} name="username" onChange={handleChange}></input>
+            <input type="password" value={loginData.password} name="password" onChange={handleChange}></input>
+            <input type="submit" value="Log In"></input>
+        </form>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        loginData: state.loginForm
+    }
+}
+
+export default connect(mapStateToProps, { updateLoginForm, login })(Login);
