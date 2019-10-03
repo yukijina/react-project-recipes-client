0d2d09a23f1d24f106ad6cf0baa9d31f8e620a02
yukijina <yukijina@gmail.com>
2019-10-03 07:20:30 -0700

    add fetch signup

--- src/actions/currentUsers.js
+++ src/actions/currentUsers.js
@@ -1,3 +1,6 @@
+import { resetSignupForm } from './signupForm.js'
+
+
 export const setCurrentUser = user => {
     return {
         type: 'SET_CURRENT_USER',
@@ -12,8 +15,34 @@ export const clearCurrentUser = () => {
 }
 
 //asynchronous
+export const signup = userData => {
+    console.log("action", userData)
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/signup", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user: userData})
+        })
+        .then(resp => resp.json())
+        .then(user =>  
+           { 
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+                dispatch(resetSignupForm())
+            }
+            
+        })
+    }
+}
+
+
+
 export const login = credentials => {
-    console.log(credentials)
     return dispatch => {
         return fetch("http://localhost:3001/api/v1/login", {
             credentials: 'include',
