930dffb4ba116b418d61af3b2f4eeb0a459b73a8
yukijina <yukijina@gmail.com>
2019-10-04 13:27:47 -0700

    add list of links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = (props) => {
     return (
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Log In</Link></li>
-                <li><Link to="/logout">Log Out</Link></li>
-                <li><Link to="/recipes">Recipes</Link></li>
-            </ul>
+        <div>   
+                <ul>
+                    <li><Link to="/login">Log In</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/recipes">Recipes</Link></li>
+                    <li><Link to="/logout">Log Out</Link></li>
+                </ul>
+            
         </div>
     )
 }
