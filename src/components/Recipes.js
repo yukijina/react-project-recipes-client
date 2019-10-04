240f2a02387b3aca0cf3cef82306b0c6495f44b2
yukijina <yukijina@gmail.com>
2019-10-04 15:35:31 -0700

    update search box function

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
-import Recipe from './Recipe.js'
+import { connect } from 'react-redux';
+import { searchRecipes } from '../actions/recipeActions.js';
+//import Recipe from './Recipe.js'
 
 
 class Recipes extends Component {
+    state = {
+        query: ""
+    }
+
+    handleInputChange = event => {
+        // searchQuery({...this.props.query, [event.target.name]: event.target.value})
+        this.setState({
+            query: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.propssearchRecipes(this.state.query)
+    }
+
     render() {
-        console.log(this.props.recipes)
+        //console.log(this.props.recipes)
         return(
             <div>
+                <h1>Today's choice</h1>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Search or leave blank:
+                        <input type="text" name="query" value={this.state.query} placeholder="e.g.burger" onChange={this.handleInputChange}></input>
+                    </label>
+    
+                    <label>
+                    Pick your favorite diet:
+                        <select onChange={this.handleSelectChange}>
+                            <option>Select</option>
+                            <option value="all">All</option>
+                            <option value="glutenfree">Gluten Free</option>
+                            <option value="ketgenic">Ketogenic</option>
+                            <option value="vegetarian">Vegetarian</option>
+                            <option value="vegn">Vegan</option>
+                            <option value="paleo">Paleo</option>
+                            <option value="whole30">Whole 30</option>
+                        </select>
+                    </label>
+                    <input type="submit" value="Submit"></input>
+                </form>
+
+
                 {this.props.recipes !== null? this.props.recipes.map(recipe => <div key={recipe.recipeId}><h2 >{recipe.title}</h2><img src={recipe.image}></img></div>) : null}
+
+        
+
             </div>
         )
     }
 }
 
-export default Recipes;
+
+
+
+export default connect(null, {searchRecipes})(Recipes);
