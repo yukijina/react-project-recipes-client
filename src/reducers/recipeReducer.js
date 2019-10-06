7509ff32a905eff5d019146f2d66378ec89925ad
yukijina <yukijina@gmail.com>
2019-10-06 16:43:31 -0700

    add RESET_RECIPE

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,10 +1,18 @@
 export default (state = {
     recipe: null,
+    favorite: 0
 }, action) => {
     console.log("RecipeReducer:", action.type, action.payload)
     switch(action.type) {
-        case 'INDIVIDUAL_RECIPE':
-            return {recipe: action.payload}
+        case 'UPLOADING_RECIPE':
+            return {...state, recipe: action.payload}
+
+        case 'INCREMENT_FAVORITE':
+            return {...state, favorite: +1 }
+        
+        case 'RESET_RECIPE':
+            return {recipe: null, favorite: 0}
+            
         default:
             return state
     }
