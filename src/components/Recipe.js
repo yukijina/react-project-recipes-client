d12a55bd92d7683cafc0992e5cbb1b2ac4a0100c
yukijina <yukijina@gmail.com>
2019-10-06 10:25:00 -0700

    change mapStateToProps. Add clikck like button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Recipe extends Component {
+    handleClick = (recipeId) => {
+        console.log("like btn:", recipeId)
+    }
+
     render() {
         return(
-            <div></div>
+            <div className="Recipe">
+                <div>
+                <h2>{this.props.recipe.title}</h2> 
+                <img src={this.props.recipe.image}></img>
+                </div>
+                <button onClick={() => this.handleClick(this.props.recipe.recipeId)}>&#10084; Like</button>
+                <p>Read in Minutes: {this.props.recipe.readyInMinutes}</p>
+                <p>Servings: {this.props.recipe.servings}</p>
+                <p>Vegetarian: {this.props.recipe.vegetarian ? "Yes" : "No" }</p>
+                <p>Vegan: {this.props.recipe.vegan ? "Yes" : "No" }</p>
+                <p>Gluten Free: {this.props.recipe.glutenfree ? "Yes" : "No" }</p>
+                <p>Dairy Free: {this.props.recipe.dairyfree ? "Yes" : "No"}</p>
+                <p>Ketogenic: {this.props.recipe.ketogenic ? "Yes" : "No" }</p>
+                <p>Whole30: {this.props.recipe.whole30 ? "Yes" : "No" }</p>
+                {/* <p>Instructions:{this.props.recipe.instructions}</p>
+                <>this.props.ingredients.map(ing => <ul><li>{ing.name}</li><li>{ing.original}</li><li>{ing.amount}: {ing.unit}</li></ul>)}</> */}
+                
+            </div>
         )
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        recipe: state.recipeReducer.recipe
+    }
+}
 
-export default Recipe
+export default connect(mapStateToProps)(Recipe);
