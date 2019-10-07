96f2022f35dfc9dc046d5a8a45a7b1a1b5dbfc68
yukijina <yukijina@gmail.com>
2019-10-07 06:04:00 -0700

    create revew input. Submit review to Rails api

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,9 +4,28 @@ import { clickLike } from '../actions/recipeActions.js';
 
 
 class Recipe extends Component {
+
+    state = {
+        review: ""
+    }
+
     handleClick = (recipe, userId) => {
         console.log("like btn:", recipe, userId)
-        this.props.clickLike(recipe, {userId})
+        this.props.clickLike(recipe, userId, this.state.review)
+    }
+
+    handleInputChange = (event) => {
+        // this.props.updateReviewForm({...this.props.review, [event.target.name]: [event.target.value]})
+        this.setState({
+            review: event.target.value
+        })
+    }
+
+    handleSubmit = (event, recipe, userId) => {
+        event.preventDefault()
+        console.log("review submit", recipe, userId, this.state.review)
+        // this.props.postingReviews(this.state.review, recipe, userId)
+        this.props.clickLike(recipe, userId, this.state.review)
     }
 
     render() {
@@ -16,7 +35,7 @@ class Recipe extends Component {
                 <h2>{this.props.recipe.title}</h2> 
                 <img src={this.props.recipe.image}></img>
                 </div>
-                <button onClick={() => this.handleClick(this.props.recipe, this.props.userId)}>&#10084; Like: {this.props.favorite}</button>
+                <button onClick={() => this.handleClick(this.props.recipe, this.props.userId, this.state.review)}>&#10084; Like: {this.props.favorite}</button>
                 <p>Write Review:</p>
                 <p>Read in Minutes: {this.props.recipe.readyInMinutes}</p>
                 <p>Servings: {this.props.recipe.servings}</p>
@@ -28,6 +47,12 @@ class Recipe extends Component {
                 <p>Whole30: {this.props.recipe.whole30 ? "Yes" : "No" }</p>
                 {/* <p>Instructions:{this.props.recipe.instructions}</p>
                 <>this.props.ingredients.map(ing => <ul><li>{ing.name}</li><li>{ing.original}</li><li>{ing.amount}: {ing.unit}</li></ul>)}</> */}
+                <h2>Reviews: </h2>
+                <form onSubmit={(event) => this.handleSubmit(event, this.props.recipe, this.props.userId)}>
+                    <input type="text" name="review" value={this.state.review} onChange={this.handleInputChange}></input>
+                    <input type="submit" value="Add review"></input>
+                </form>
+
                 
             </div>
         )
@@ -38,7 +63,9 @@ const mapStateToProps = state => {
     return {
         recipe: state.recipeReducer.recipe,
         userId: state.currentUsersReducer.id,
-        favorite: state.recipeReducer.favorite
+        favorite: state.recipeReducer.favorite,
+       // reviewData: state.recipeReducer,
+        reviews: state.recipeReducer.reviews
     }
 }
 
