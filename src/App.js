b9dd16d66c17fd8e857d9187327675c5f219dae7
yukijina <yukijina@gmail.com>
2019-10-08 20:58:56 -0700

    add protected router. create loggedin props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { fetchRecipes } from './actions/recipeActions.js';
+//import { fetchRecipes } from './actions/recipesActions.js';
 import Home from './components/Home.js';
 import NavBar from './components/NavBar.js';
 import RandomRecipesContainer from './containers/RandomRecipesContainer.js';
@@ -11,29 +11,33 @@ import Signup from './components/Signup.js';
 import { getCurrentUser } from './actions/currentUsers.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Recipe from './components/Recipe.js';
+import User from './containers/User.js';
+import { logout } from './actions/currentUsers.js';
+import ProtectedRoute from './containers/ProtectedRoute.js'
 
 class App extends Component {
-
   componentDidMount(){
     this.props.getCurrentUser()
     //this.props.fetchRecipes()
   }
 
-  
   render() {
-  
+   
     return (
       <Router>
-        {this.props.isLoggedin ? <NavBar isLoggedin= {this.props.isLoggedin} /> : <Home />}
-        <div className="App">
+        {this.props.loggedin ? <NavBar loggedin= {this.props.loggedin} /> : <Home />}
 
+        <div className="App">
           <Switch>
-            {/* <Route exact path='/' render={() => <Home isLoggedin={this.props.isLoggedin} />}/> */}
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/logout' component={Logout}/>
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/recipes' component={RandomRecipesContainer}/>
-            <Route exact path='/recipe/:id' render={() => <Recipe recipe={this.props.recipe}/>} />
+              <ProtectedRoute exact path='/logout' component={Logout}/>
+              <ProtectedRoute exact path='/recipes' component={RandomRecipesContainer}/>
+              <ProtectedRoute exact path='/recipes/:id' render={() => <Recipe recipe={this.props.recipe}/>} />
+              <ProtectedRoute exact path='/myaccount' render={() => <User currentUser={this.props.currentUser}/>} />
+   
+              <Route exact path='/' render={() => <Home loggedin={this.props.loggedin} />}/>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            
           </Switch>
         </div>
       </Router>
@@ -42,24 +46,14 @@ class App extends Component {
 }
 
 const mapStateToProps = state => {
+  const status = state.currentUsersReducer === null || state.currentUsersReducer.error ? false : true
   return {
     recipes: state.recipesReducer.recipes,
     currentUser: state.currentUsersReducer,
-    isLoggedin: !!state.currentUsersReducer
+    loggedin: status
   }
 }
 
 
-export default connect(mapStateToProps,{ getCurrentUser, fetchRecipes })(App);
-
-
-//route
-// import reactroutesr
-// import {link, Route}
-// /  (home)
-//search - recipe results (external api)
-//favorite  (internal api)
-//recipe  (external/internal api)
-//user show   favorite, review - model
-
-// recipe url, image, title, favor
+// export default connect(mapStateToProps,{ getCurrentUser, fetchRecipes })(App);
+export default connect(mapStateToProps,{ getCurrentUser, logout })(App);
