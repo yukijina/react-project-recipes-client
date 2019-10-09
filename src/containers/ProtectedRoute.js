f5d9d28fd6a1b5bee5b77de0ec2bba46ac1357ea
yukijina <yukijina@gmail.com>
2019-10-08 20:59:32 -0700

    create protectedRoute only logged in user can see protected routers

---
+++ src/containers/ProtectedRoute.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ loggedin, component: Component, ...props}) => {
+    return (
+        <Route {...props}
+        render={property => {
+            console.log(loggedin)
+            if (loggedin) {
+                return <Component {...property} />
+            } else {
+                return <Redirect to={
+                    {
+                        pathname: "/",
+                        state: {
+                            from: property.location
+                        }
+                    } 
+                }/>
+            }
+        }} />
+    )
+}
+
+const mapStateToProps = state => {
+    const status = state.currentUsersReducer === null || state.currentUsersReducer.error ? false : true
+    return {
+      loggedin: status
+    }
+  }
+
+export default connect(mapStateToProps)(ProtectedRoute);
+//export default ProtectedRoute;
