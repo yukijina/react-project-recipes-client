e6e8e6d5019c856d96e44f8a7974bb3647e32009
yukijina <yukijina@gmail.com>
2019-10-09 21:45:00 -0700

    add componentDidMount

--- src/containers/RandomRecipesContainer.js
+++ src/containers/RandomRecipesContainer.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Recipes from '../components/Recipes.js';
+import { fetchRecipes } from '../actions/recipesActions.js';
 
 
 class RandomRecipesContainer extends Component {
-    render() {
+    componentDidMount(){
+        this.props.fetchRecipes()
+    }
 
+    render() {
         return (
             <div>
-                <Recipes recipes={this.props.recipes} />
+                <Recipes recipes={this.props.recipes} hisotry={this.props.history} />
                 
             </div>
         )
     }
 }
 
-export default connect((state)=>({recipes: state.recipesReducer.recipes}))(RandomRecipesContainer)
+
+export default connect((state)=>({recipes: state.recipesReducer.recipes}), { fetchRecipes })(RandomRecipesContainer)
