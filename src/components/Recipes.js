8f656c0b1b1f934c495eff712f237e73aa84d7e8
yukijina <yukijina@gmail.com>
2019-10-10 21:21:15 -0700

    add css. import material-ui

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { searchRecipes } from '../actions/recipesActions.js';
 import { recipeShow, loadingFavorite } from '../actions/recipeActions.js';
-import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
-
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 
 class Recipes extends Component {
@@ -46,12 +49,12 @@ class Recipes extends Component {
         //console.log(this.props.recipes)
         return(
             <div className="Recipes">
-                <h1>Today's choice</h1>
+                <h1 style={{color: "#555"}}>Today's choice</h1>
                 
-                <form onSubmit={this.handleSubmit} style={{marginTop: "6%"}}>
+                <form onSubmit={this.handleSubmit} style={{margin: "6% auto"}}>
                     Search : <TextField type="text" name="query" value={this.state.query} placeholder="e.g.burger" onChange={this.handleInputChange} style={{marginLeft: "10px", marginRight: "10px"}} />
                 
-                    Pick your favorite diet:
+                    Diet:
                         <Select value={this.state.diet} onChange={this.handleSelectChange}>
                             <MenuItem>Select</MenuItem>
                             <MenuItem value="all">All</MenuItem>
@@ -63,13 +66,16 @@ class Recipes extends Component {
                             <MenuItem value="whole30">Whole 30</MenuItem>
                         </Select>
                     
-                   
                     <input type="submit" value="Search" className="btn btn-full" style={{marginLeft: "15px"}}></input>
-                    </form>
+                </form>
 
-
-                {this.props.recipes !== null ? this.props.recipes.map(recipe => <div key={recipe.recipeId}><h2>{recipe.title}</h2><img src={recipe.image} onClick={() => this.handleClick(recipe.recipeId, this.props.hisotry)}></img></div>) : null}     
-
+                <GridList cols={2}>
+                    {this.props.recipes !== null ? this.props.recipes.map(recipe => <GridListTile key={recipe.recipeId}><img src={recipe.image}></img><GridListTileBar title={recipe.title}  actionIcon={
+                        <IconButton onClick={() => this.handleClick(recipe.recipeId, this.props.hisotry)}>
+                        <MoreHorizIcon style={{color: "#FCF3F3"}}  />
+                        </IconButton>
+              }></GridListTileBar></GridListTile>) : null}     
+                </GridList>
             </div>
         )
     }
