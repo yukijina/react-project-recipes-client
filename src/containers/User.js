b781b27200f116ed37ec2f6f3ba654a7c477e5e2
yukijina <yukijina@gmail.com>
2019-10-12 10:28:13 -0700

    import material-ui. Change tag element for css

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { recipeShow } from '../actions/recipeActions';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 class User extends Component {
 
@@ -17,7 +23,7 @@ class User extends Component {
             if(recipes){
                 console.log(recipes)
             favoriteText = (
-                recipes.map(recipe => <li><a href="#" onClick={this.handleClick} data-apiId={recipe.api_id}>{recipe.title}</a></li>)
+                recipes.map(recipe => <ListItem alignItems="flex-start"><ListItemAvatar><Avatar src={recipe.image} /></ListItemAvatar><ListItemText primary={`${recipe.title.substring(0,25)}...`} /><a href="#" onClick={this.handleClick} data-apiId={recipe.api_id}>More</a><Divider variant="inset" component="li" /></ListItem>)
             )
             } else {
              favoriteText = ("You don't have any favorite recipes yet.")
@@ -26,9 +32,8 @@ class User extends Component {
 
         return (
             <div className="MyAccount">
-                <h2>My Account</h2>
-                 {user ? <div><h2>Hello, {user.username}</h2><h3>Your Favorite Recipes:</h3></div> : null}
-                <ul>{favoriteText}</ul>
+                 {user ? <div><h1>Hello, {user.username}</h1><h2>Your Favorite Recipes:</h2></div> : null}
+                <List>{favoriteText}</List>
                 
             </div>
         )
