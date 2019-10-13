7fa6c8d1fec17311f2f2568ea74e1e7580a32074
yukijina <yukijina@gmail.com>
2019-10-13 01:24:11 -0700

    add UserReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import recipeReducer from './recipeReducer.js';
 import currentUsersReducer from './currentUsersReducer.js';
 import loginForm from './loginForm.js'
 import signupForm from './signupForm.js'
+import userReducer from './userReducer.js'
 
 const rootReducer = combineReducers({
     recipesReducer,
     currentUsersReducer,
     loginForm,
     signupForm,
-    recipeReducer
+    recipeReducer,
+    userReducer
 })
 
 export default rootReducer
