9c8d874a8c8eecae15b9437ca8b193617abec2c7
yukijina <yukijina@gmail.com>
2019-10-12 19:29:58 -0700

    clean the codes

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,34 +71,3 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps, { updateLoginForm, login })(Login);
 
 
-
-// class Login extends Component {
-
-//     handleChange = event => {
-//         this.props.updateLoginForm({...this.props.loginData, [event.target.name]: event.target.value})
-//     }
-    
-//     handleSubmit = event => {
-//         event.preventDefault()
-//         this.props.login(this.props.loginData, this.props.history)
-//     }
-    
-//     render(){
-        
-//     return (    
-//         <form onSubmit={this.handleSubmit}>
-//             <input type="text" value={this.props.loginData.username} name="username" onChange={this.handleChange}></input>
-//             <input type="password" value={this.props.loginData.password} name="password" onChange={this.handleChange}></input>
-//             <input type="submit" value="Log In"></input>
-//         </form>
-//     )
-//     }
-// }
-
-// const mapStateToProps = state => {
-//     return {
-//         loginData: state.loginForm
-//     }
-// }
-
-// export default connect(mapStateToProps, { updateLoginForm, login })(Login);
