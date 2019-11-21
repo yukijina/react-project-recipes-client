8606ef7b95e2ed7a2b63164f9c6a430fe79b930c
yukijina <yukijina@gmail.com>
2019-11-20 16:50:36 -0800

    add url env variable

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -9,7 +9,7 @@ export const uploadingFavorite = recipes => {
 export const loadingUserInfo = (currentUserId) => {
     const HEROKU_URL = process.env.REACT_APP_HEROKU
     return (dispatch) => {
-        return fetch(`${HEROKU_URL}/api/v1/users/${currentUserId}` ,{
+        return fetch(`http://localhost:3001/api/v1/users/${currentUserId}` ,{
             credentials: "include",
             method: "GET",
             headers: {
